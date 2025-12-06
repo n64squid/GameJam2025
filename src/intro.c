f13697b9d0ee0a6db04b9c65bd08368e494238d4
N64 Squid <squid@n64squid.com>
2025-12-06 02:20:16 +0000

    Added some placeholder functions

---
+++ src/intro.c
@@ -0,0 +1,17 @@
+#include "intro.h"
+
+void intro_init (void) {
+
+}
+
+void intro_move (void) {
+
+}
+
+void intro_draw (void) {
+
+}
+
+void intro_clean (void) {
+
+}
--- src/intro.h
+++ src/intro.h
@@ -1,4 +1,11 @@
 #ifndef GAMEJAM2025_INTRO_H
 #define GAMEJAM2025_INTRO_H
 
+#include "main.h"
+
+void intro_init (void);
+void intro_move (void);
+void intro_draw (void);
+void intro_clean (void);
+
 #endif
